5c183e6e6acc08113cf1defc504616c72bfa56f5
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-02 17:51:14 -0400

    Exercise 3 Complete

--- server.js
+++ server.js
@@ -35,11 +35,20 @@ express()
       '🙉',
     ];
     const randomN = Math.floor(Math.random() * 6);
-    const message = {author: 'cat', text: messages[randomN]};
+    const message = {author: 'monkey', text: messages[randomN]};
     const randomTime = Math.floor(Math.random() * 3000);
     setTimeout(() => {
     res.status(200).json({status: 200, message, messages})
     }, randomTime);
+  })
+
+  .get(`/parrot-message/`, (req, res) => {
+    const message = {author: 'parrot', text: req.query.msg};
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+    res.status(200).json({status: 200, message})
+    }, randomTime);
+    console.log(req.query)
   })
   
   // add new endpoints here ☝️
